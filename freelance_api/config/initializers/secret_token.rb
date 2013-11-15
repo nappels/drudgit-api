02a83f77e05ffe6b55cfb8787e174b9f813058ee
Noah Appel <nappel@aquto.com>
2013-11-14 21:05:49 -0500

    Created new rails app

---
+++ freelance_api/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FreelanceApi::Application.config.secret_key_base = '403f0bad6b4e959ea831195dbe15fd3bce09a689586a73d7a2bfc53b321c49ad415cf8448e04bc10b740591146bb4ebdddf781770fea9ab5039679227bb09ae9'
